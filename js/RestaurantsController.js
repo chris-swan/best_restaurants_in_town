b79d93c380b03bff4b4fa261dc68b3e8d081df33
Chris Swan & Filipe Silvestre <student@epicodus.com>
2015-09-21 11:48:14 -0700

    fix forms to clear after input is entered and submitted.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -4,7 +4,10 @@ restaurant.controller('RestaurantsCtrl', function RestaurantCtrl($scope) {
 
   $scope.addRestaurant = function() {
     $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, address: $scope.restaurantAddress, contact: $scope.restaurantContact});
-    $scope.restuarantName = null;
+    $scope.restaurantName = null;
+    $scope.restaurantCuisine = null;
+    $scope.restaurantAddress = null;
+    $scope.restaurantContact = null;
   };
 
   $scope.deleteRestaurant = function(restaurant) {
