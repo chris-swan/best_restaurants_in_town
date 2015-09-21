ff245737f5b349b2740f06de20f0080bc3284cee
Chris Swan & Filipe Silvestre <student@epicodus.com>
2015-09-21 15:01:31 -0700

    add search check box to filter results.

--- js/RestaurantsController.js
+++ js/RestaurantsController.js
@@ -3,12 +3,14 @@ restaurant.controller('RestaurantsCtrl', function RestaurantCtrl($scope) {
   $scope.restaurants = [];
 
   $scope.addRestaurant = function() {
-    $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, address: $scope.restaurantAddress, contact: $scope.restaurantContact, priceRange: $scope.restaurantPriceRange });
+    $scope.restaurants.push({ name: $scope.restaurantName, cuisine: $scope.restaurantCuisine, address: $scope.restaurantAddress, contact: $scope.restaurantContact, priceRange: $scope.restaurantPriceRange, kidFriendly: $scope.kidFriendly, takeOut: $scope.takeOut});
     $scope.restaurantName = null;
     $scope.restaurantCuisine = null;
     $scope.restaurantAddress = null;
     $scope.restaurantContact = null;
     $scope.restaurantPriceRange = null;
+    $scope.restaurantKidFriendly = null;
+    $scope.restaurantTakeOut = null;
   };
 
   $scope.deleteRestaurant = function(restaurant) {
